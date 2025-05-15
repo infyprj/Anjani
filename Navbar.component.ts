b36f894ffdb0b79566e195ea9d88e20648e98bf3
infyprj <naturalmovies267@gmail.com>
2025-05-15 16:11:32 +0530

    Navbar.component.ts

---
+++ Navbar.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html'
+})
+export class NavbarComponent {
+  // Optional: implement login status, user info, etc.
+}
