a98b894ea2c7c05e768d27b3238d30288721bee7
infyprj <naturalmovies267@gmail.com>
2025-05-16 10:03:54 +0530

    Delete Navbar.component.ts

--- Navbar.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html'
-})
-export class NavbarComponent {
-  // Optional: implement login status, user info, etc.
-}
